4ff2d5d1e00e9a8fc362666d67f31e78bf309bd3
pete <peterhollobon@yingguo.local>
2009-08-03 10:44:56 +0100

    add more details to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('bitset',
-                    sources = ['bitsetmodule.c'])
+bitset_extmodule = Extension('bitset',
+                             sources=['bitsetmodule.c'])
 
-setup (name = 'PackageName',
-       version = '1.0',
-       description = 'This is a demo package',
-       ext_modules = [module1])
+setup(name='bitset',
+      version='0.1',
+      description='Bitset module.',
+      ext_modules=[bitset_extmodule],
+      author="Pete Hollobon",
+      author_email="python@hollobon.com",
+      url="http://github.com/hollobon/pybitset",
+      licence="MIT"
+      )
 
